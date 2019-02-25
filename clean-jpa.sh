221813a9cd57f0d4bc56900a3dc7a07963dd03ac
myapit <hafeez.cs@gmail.com>
2019-02-25 10:42:08 +0800

    Update clean-jpa.sh
    update clean jpa. tambah more options

--- clean-jpa.sh
+++ clean-jpa.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 find /var/www/html/ -name "*.jpa" | grep backup | xargs rm
+find /var/www/html/ -name "*.j01" | grep backup | xargs rm
+find /var/www/html/ -name "*.zip" | grep "wp-content" | grep backups | xargs rm
+
