f77f8a95df7ecfd1f43987d6bb0e8c5fe2088008
hafeez <hafeez.cs@gmail.com>
2019-03-14 16:21:21 +0800

    Update git-create.sh

--- git-create.sh
+++ git-create.sh
@@ -3,7 +3,7 @@
 repo_name=$1
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
 
-curl -u 'myapit' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+curl -u '<your_user_name>' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 
 
 git init
