bc389c2f90f31f55ffa3096d1463bf282e4bd785
hafeez <hafeez.cs@gmail.com>
2019-03-14 16:21:05 +0800

    Update git-create.sh

--- git-create.sh
+++ git-create.sh
@@ -4,3 +4,7 @@ repo_name=$1
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
 
 curl -u 'myapit' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+
+
+git init
+git remote add origin "https://github.com/your_github_username/$repo_name.git"
